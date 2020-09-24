4c121c8ed03b4f725de0acf25fe84748947f2638
samsemasen <sam.semasen@gmail.com>
2020-09-24 23:32:06 +0300

    first commit

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+	private readonly charmenderAttacks _attackType;
+
+	public Attacker(charmenderAttacks attackType)
+	{
+		_attackType = attackType;
+	}
+
+	public void Attack()
+	{
+		Debug.Log("attack " + _attackType);
+	}
+}
+
+enum charmenderAttacks
+{
+	FireBlast,
+	Slash,
+	WingAttack,
+	FireStorm
+}
